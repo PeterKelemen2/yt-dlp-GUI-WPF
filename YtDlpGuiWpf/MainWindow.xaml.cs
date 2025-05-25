7296c3a120d13ad12971bf742606973b537a4e67
Péter Kelemen <peter.kelemen456@gmail.com>
2025-05-25 11:51:36 +0200

    Made the title bar use dark mode

--- YtDlpGuiWpf/MainWindow.xaml.cs
+++ YtDlpGuiWpf/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.Json;
 using System.Windows;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -227,9 +229,22 @@ public partial class MainWindow : Window
         }
     }
 
+    private enum DWMWINDOWATTRIBUTE : uint
+    {
+        DWMWA_USE_IMMERSIVE_DARK_MODE = 20
+    }
+
+    [DllImport("dwmapi.dll")]
+    private static extern int DwmSetWindowAttribute(IntPtr hwnd, DWMWINDOWATTRIBUTE attr, ref bool attrValue,
+        int attrSize);
 
     private void MainWindow_Loaded(object sender, RoutedEventArgs e)
     {
+        var hwnd = new WindowInteropHelper(this).Handle;
+        var attribute = DWMWINDOWATTRIBUTE.DWMWA_USE_IMMERSIVE_DARK_MODE;
+        var useDarkMode = true;
+        DwmSetWindowAttribute(hwnd, attribute, ref useDarkMode, Marshal.SizeOf(typeof(bool)));
+
         if (File.Exists(YtdlpSettings.ConfigPath))
         {
             try
